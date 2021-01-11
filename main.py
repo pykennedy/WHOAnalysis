4c1610d6151ec4c9168ce0406d1dc55e3aab159d
Peter Kennedy <pykennedy@gmail.com>
2021-01-10 16:05:19 -0800

    testing 123

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import numpy as np
+import pandas as pd
+import tabulate as tb
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+pd.options.display.max_columns = None
+pd.options.display.width = None
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def as_table(dataframe):
+    return tb.tabulate(dataframe, 'firstrow', 'psql')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+df = pd.read_csv("data/30-70cancerChdEtc.csv")
+print(as_table(df.head()))
